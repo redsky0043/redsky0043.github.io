bec25193218cc582af59543df76f3cae3368bf6c
Boiko Serhii <boyko0043@gmail.com>
2020-06-18 12:09:47 +0300

    upload H6

---
+++ Web_Academy/H6/script.js
@@ -0,0 +1,60 @@
+// Task#1
+
+// let admin;
+// let name = 'John';
+
+// admin = name;
+
+// alert(admin);
+
+
+
+
+/* Task #2 
+
+    1. hello 1
+    2. hello name
+    3. hello ilya
+
+    *написано без участі JS-ка))
+*/
+
+
+
+// Task#3
+
+// let userNum = prompt('write a number', '');
+
+// if (userNum > 0) {
+//     alert(1)
+// } else if (userNum == 0) {
+//     alert(0)
+// } else {
+//     alert(-1)
+// };
+
+
+
+
+// Task#4
+
+let age = +prompt('write a number', '');
+
+console.log(age);
+
+if ((age >= 14) && (age <= 90)) {
+    alert(true)
+} else {
+    alert(false)
+}
+
+
+
+
+// Task#5
+
+// let i = 0;
+
+// for (i <= 10 ) {
+//     alert
+// }
