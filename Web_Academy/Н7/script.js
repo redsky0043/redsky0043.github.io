2f092fd166124531f8eef79c2954de66db111e5c
Boiko Serhii <boyko0043@gmail.com>
2020-06-19 14:57:06 +0300

    add H7

---
+++ Web_Academy/Н7/script.js
@@ -0,0 +1,15 @@
+function makeCounter() {
+    function sdsd() {
+        
+    }
+}
+
+let counter = makeCounter();
+let counter2 = makeCounter();
+
+alert( counter() );
+alert( counter() );
+alert( counter2() );
+alert( counter(2) );
+alert( counter() );
+
