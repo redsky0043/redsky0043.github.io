4b901fa9649fead6e476fab53b55cf6ac21e5b17
Serhii Boiko <boyko0043@gmail.com>
2020-06-25 18:33:33 +0300

    H8_2 is done

--- Web_Academy/Н8/Task_2/js/Task1.js
+++ Web_Academy/Н8/Task_2/js/Task1.js
@@ -1,39 +1,104 @@
 
 let arr = [
-    { operation: '1+2+(3/3)', result: '4', },
-    { operation: '2+3', result: '5', },
-    { operation: '2+2*3', result: '8', },
-    { operation: '1+1+1+1+1', result: '5', },
-    { operation: '9/3', result: '3', },
+    { 
+        user: 'Web Academy', 
+        message: 'Хай) Як успіхи з pеактом?', 
+        time: '16:35',
+        day: 'Вт', 
+        isMyMessage: true, },
+    { 
+        user: 'Serhii Boiko', 
+        message: 'нормас)', 
+        time: '16:36',
+        day: 'Ср', 
+        isMyMessage: false ,},
+    { 
+        user: 'Web Academy', 
+        message: 'Треба допомога?', 
+        time: '16:37',
+        day: 'Чт', 
+        isMyMessage: true, },
+    { 
+        user: 'Serhii Boiko', 
+        message: 'Не завадить', 
+        time: '16:38',
+        day: 'Пт', 
+        isMyMessage: false, },
+    { 
+        user: 'Web Academy', 
+        message: 'Зараз все зробим)', 
+        time: '16:39',
+        day: 'Сб', 
+        isMyMessage: true, },
+    { 
+        user: 'Serhii Boiko', 
+        message: '&#128525; &#128525; &#128525;', 
+        time: '16:40',
+        day: 'Вс', 
+        isMyMessage: false,
+    },
 ];
 
+
+
+// функция для сообщений
+
 const renderList = smt => {
-   if ( smt.length == 0 ) {
-    list.innerHTML = `
-    <div class="modal__alert">
-        calculation results will be displayed here
-    </div>
-    `;
-   }
-   else {
+
     let listHTML = "";
     smt.forEach(item => {
         listHTML += `
-        <div class="modal__row">
-            <div class="modal__row__operation style">
-                ${item.operation}
+            <div class="${item.isMyMessage ? "user__message" : "user__message2"}">
+                <div class="user__message__text">
+                    ${item.message}
+                </div>
+                <div class="user__message__time">
+                    ${item.time}
+                </div>
+                <div class="user__message__name">
+                    ${item.user}
+                </div>
             </div>
-            <div class="modal__row__sym style">
-                =
-            </div>
-            <div class="modal__row__result style">
-                ${item.result}
-            </div>
-        </div>
         `;
     });
     list.innerHTML = listHTML;
    }
-};
+;
 
 renderList(arr);
+
+
+
+
+// функция для сайдбара
+
+const renderSidebar = smt => {
+
+    let listHTML = "";
+    smt.forEach(item => {
+        listHTML += `
+        <div class="sidebar__messages">
+            <div class="sidebar__message">
+                <img class="sidebar__message__img" src=" ${item.isMyMessage ? "img/wa.png" : "img/sb.jpg" } ">
+                <div class="sidebar__message__body">
+                    <div class="sidebar__message__title">
+                        <div class="sidebar__meassage__name">
+                            ${item.user}
+                        </div> 
+                        <div class="sidebar__meassage__day">
+                            ${item.day}
+                        </div>
+                    </div>
+                    <div class="sidebar__message__text">
+                        ${item.message}
+                    </div>
+                </div>
+            </div>   
+        </div>
+        `;
+    });
+    side.innerHTML = listHTML;
+   }
+;
+
+renderSidebar(arr);
