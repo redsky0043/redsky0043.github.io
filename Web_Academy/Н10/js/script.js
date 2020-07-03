f638856bbd4fc2c8980a87c959df0771714754c4
Serhii Boiko <boyko0043@gmail.com>
2020-07-03 19:47:28 +0300

    H10

---
+++ Web_Academy/Н10/js/script.js
@@ -0,0 +1,42 @@
+     //-------------1------------------   
+     
+        // printNumbers = (from, to) => {
+
+        //     let interval = setInterval(() => {
+        //         console.log(from);
+
+        //         if (from == to) {
+        //             clearInterval(interval);
+        //         }
+
+        //         from++;
+        //     }, 1000);
+        // }
+        // printNumbers (3, 9);
+
+    //-------------1------------------ 
+
+
+
+
+
+
+
+
+    //-------------2------------------ 
+
+    // printNumbers = (from, to, time) => {
+
+    //         setTimeout( interval = () => {
+    //             console.log(from);
+    //             if (from < to) {   
+    //                 setTimeout(interval, time);
+    //             }   
+    //             from++;
+    //         }, 0);
+    // }
+
+    // printNumbers(1, 20, 100);
+  
+
+    //-------------2------------------ 
---
+++ Web_Academy/Н10/script.js
@@ -0,0 +1,10 @@
+function main() {
+    console.log("another file JS");
+
+//     setTimeout(() => {
+//         console.log("B");
+//     }, 0);
+
+//     console.log("C");
+}
+
