11a8b684ce5201c485e2454383215fd3b2881cce
Boiko Serhii <boyko0043@gmail.com>
2020-07-03 22:35:42 +0300

    done first part of h10

--- Web_Academy/Н10/js/script.js
+++ Web_Academy/Н10/js/script.js
@@ -40,3 +40,48 @@
   
 
     //-------------2------------------ 
+
+
+
+
+
+
+
+
+    //-------------3------------------
+
+     // основная функция
+    notify = (time, text) => {
+        let btn = document.querySelector("button");
+        let close = document.querySelector(".close");
+        let window = document.querySelector(".notification");
+        let mes = document.querySelector(".message");
+        let listHTML = "";
+    // открытие окна по клику и закрытие через время
+        btn.addEventListener("click", () => {
+            window.style.display = "inherit";
+            setTimeout(function(){
+                window.style.display = "none"; 
+            }, time);
+        });
+
+        listHTML += `
+            <div class="text">
+                ${text}
+            </div>
+        `;
+    // функция закрытия по клику на крестик       
+        turnOffNotification = () => {
+            close.addEventListener("click", () => {
+                window.style.display = "none";
+            });
+        };
+
+        turnOffNotification();
+        mes.innerHTML = listHTML;
+    };
+
+    notify(6000, 'How are you?');
+
+
+    //-------------3------------------
