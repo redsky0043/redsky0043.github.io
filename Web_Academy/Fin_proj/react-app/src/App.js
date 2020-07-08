f131479a0e9bd04e7d0c613486622472742f6436
Serhii Boiko <boyko0043@gmail.com>
2020-07-08 13:33:10 +0300

    react start

--- Web_Academy/Fin_proj/react-app/src/App.js
+++ Web_Academy/Fin_proj/react-app/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = "bc31b63e051c96fb9f519beb4a7baa59";
+
+class App extends React.Component {
+
+    gettingWeather = async () => {
+        const api_url = await 
+            fetch(`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}unnits=metric`);
+        const data = await api_url.json();
+        console.log(data);
+    }
+
+    render() {
+        return (
+            <div>
+                <Info />
+                <Form />
+                <Weather /> 
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ Web_Academy/Fin_proj/react-app/src/components/info.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Weather application</h2>
+                <p>Get weather in your city</p>
+            </div>
+        );
+    }
+}
+
+export default Info;
