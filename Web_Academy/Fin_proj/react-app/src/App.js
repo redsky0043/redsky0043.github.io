20a4756c4665bf7f87c5799b47e8dfc486f40e31
Serhii Boiko <boyko0043@gmail.com>
2020-07-09 13:36:33 +0300

    H12

--- Web_Academy/Fin_proj/react-app/src/App.js
+++ Web_Academy/Fin_proj/react-app/src/App.js
@@ -7,13 +7,37 @@ const API_KEY = "bc31b63e051c96fb9f519beb4a7baa59";
 
 class App extends React.Component {
 
+    state = {
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        pressure: undefined,
+        sunset: undefined,
+        error: undefined,
+    }
+
     gettingWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
         const api_url = await 
             fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&unnits=metric`);
         const data = await api_url.json();
-        console.log(data);
+
+        var sunset = data.sys.sunset;
+        var date = new Date();
+        date.setTime(sunset);
+        var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+        if(city) {
+            this.setState({
+                temp: data.main.temp,
+                city: data.name, 
+                country: data.sys.country,
+                pressure: data.main.pressure,
+                sunset: sunset_date,
+                error: "", 
+            }); 
+        }
     }
 
     render() {
@@ -21,7 +45,14 @@ class App extends React.Component {
             <div>
                  <Info />
                 <Form weatherMethod={this.gettingWeather}/>
-                <Weather /> 
+                <Weather 
+                    temp={this.state.temp}
+                    city={this.state.name} 
+                    country={this.state.country}
+                    pressure={this.state.pressure}
+                    sunset={this.state.sunset}
+                    error={this.state.error}
+                /> 
             </div>
         );
     }
--- Web_Academy/Fin_proj/react-app/src/components/weather.js
+++ Web_Academy/Fin_proj/react-app/src/components/weather.js
@@ -4,7 +4,14 @@ class Weather extends React.Component {
     render() {
         return (
             <div>
-                Component with weather information
+                {/* { this.props.city &&
+                    <div> */}
+                        <p>Location: {this.props.city}, {this.props.country}</p>
+                        <p>Temperature: {this.props.temp}</p>
+                        <p>Pressure: {this.props.pressure}</p>
+                        <p>Sunset: {this.props.sunset}</p>
+                    {/* </div>
+                } */}
             </div>
         );
     }
