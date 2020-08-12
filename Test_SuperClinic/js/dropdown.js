25632fdc32d27d7dd82a5297442d44c5ce2e76df
Serhii Boiko <boyko0043@gmail.com>
2020-08-12 14:40:44 +0300

    v3

---
+++ Test_SuperClinic/js/dropdown.js
@@ -0,0 +1,7 @@
+dropDownOpen = () => {
+    const dropBack = document.querySelector('.drop-down').style.display='inherit';
+}
+
+dropDownClose = () => {
+    const dropBack = document.querySelector('.drop-down').style.display='none';
+}
