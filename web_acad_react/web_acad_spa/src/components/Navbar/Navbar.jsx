430551069c8aff4d67bd4ce44e3797a815e80b07
Serhii Boiko <boyko0043@gmail.com>
2020-08-26 20:44:26 +0300

    unsplash api

--- web_acad_react/web_acad_spa/src/components/Navbar/Navbar.jsx
+++ web_acad_react/web_acad_spa/src/components/Navbar/Navbar.jsx
@@ -17,4 +17,4 @@ export class Navbar extends React.Component {
             </nav>
         )
     }
-} 
+}  
--- web_acad_react/web_acad_spa/src/components/SearchForm/SearchForm.jsx
+++ web_acad_react/web_acad_spa/src/components/SearchForm/SearchForm.jsx
@@ -2,11 +2,40 @@ import React from 'react';
 import './SearchForm.scss';
 
 export class SearchForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isFocus: false,
+            searchValue: '',
+        }
+        this.submitForm = this.submitForm.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    submitForm(e) {
+        e.preventDefault()
+    }
+
+    onChange(e) {
+        const searchValue = e.currentTarget.value;
+        this.setState({ searchValue })
+    }
+
     render() {
+
+        const className = this.props.className || '';
+
         return (
-            <form className='search-form' >
-                <input className='search-form__input' type="text" placeholder='Search...' />
+            <form className={className + ' search-form'} onSubmit={this.submitForm} >
+                <button className="search-form__button">S</button>
+                <input 
+                    className='search-form__input' 
+                    type="text" 
+                    placeholder='Search...'
+                    onChange={ this.onChange } 
+                    value={ this.state.searchValue } 
+                />
             </form>
         )
     }
-}
+} 
