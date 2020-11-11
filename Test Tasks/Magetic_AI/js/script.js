572eb64aa975e1ac256a5145edc2ff23c5fcf221
Serhii Boiko <boyko0043@gmail.com>
2020-11-11 12:59:12 +0200

    deleted test project

--- Test Tasks/Magetic_AI/js/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.sidebar').onclick = function () {
-    document.querySelector('.sidebar-opened').classList.add("sidebar-opened--active");
-}
-
-document.querySelector('.sidebar-header__close-btn').onclick = function () {
-    document.querySelector('.sidebar-opened').classList.remove("sidebar-opened--active");
-}
