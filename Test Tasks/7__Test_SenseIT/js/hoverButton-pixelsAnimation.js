a67b70181404f79f016dfa7bc9d0f406e3f9547e
Serhii Boiko <boyko0043@gmail.com>
2020-12-07 15:41:23 +0200

    fixed adaptability

--- Test Tasks/7__Test_SenseIT/js/hoverButton-pixelsAnimation.js
+++ Test Tasks/7__Test_SenseIT/js/hoverButton-pixelsAnimation.js
@@ -1,7 +1,7 @@
 const allMainPixels = document.querySelectorAll('.main__pixel');
 const allBottomPixels = document.querySelectorAll('.bottom__pixel');
 
-const listClassesForApplyBottom = [
+const listClassesForApplyButton_main = [
     'first_pixel',
     'second_pixel',
     'third_pixel',
@@ -12,15 +12,20 @@ const listClassesForApplyBottom = [
     'eighth_pixel',
     'nineth_pixel',
     'tenth_pixel',
+    'eleventh_pixel',
+];
+
+const listClassesForApplyButton_bottom = [
     'twelfth_pixel',
     'thirteenth_pixel',
     'fourteenth_pixel',
     'fifteenth_pixel',
     'sixteenth_pixel',
+    'seventeenth_pixel',
     'eighteenth_pixel',
 ];
 
-const listClassesForJuryBottom = [
+const listClassesForJuryButton_main = [
     'first_pixel_2',
     'second_pixel_2',
     'third_pixel_2',
@@ -31,44 +36,55 @@ const listClassesForJuryBottom = [
     'eighth_pixel_2',
     'nineth_pixel_2',
     'tenth_pixel_2',
+    'eleventh_pixel_2',
+];
+
+const listClassesForJuryButton_bottom = [
     'twelfth_pixel_2',
     'thirteenth_pixel_2',
     'fourteenth_pixel_2',
     'fifteenth_pixel_2',
     'sixteenth_pixel_2',
+    'seventeenth_pixel_2',
     'eighteenth_pixel_2',
 ];
 
-// const addClassesForApplyBottom = allMainPixels.forEach((item, i) => {
-//     item.classList.add(listClassesForApplyBottom[i])
-// });
-
-// const removeClassesForApplyBottom = allMainPixels.forEach((item, i) => {
-//     item.classList.add(listClassesForApplyBottom[i])
-// });
-
-btnApplication.addEventListener('mouseover', () => {
+btnApply.addEventListener('mouseover', () => {
     allMainPixels.forEach((item, i) => {
-        item.classList.add(listClassesForApplyBottom[i])
+        item.classList.add(listClassesForApplyButton_main[i])
+    });
+
+    allBottomPixels.forEach((item, i) => {
+        item.classList.add(listClassesForApplyButton_bottom[i])
     });
 });
 
-btnApplication.addEventListener('mouseout', () => {
+btnApply.addEventListener('mouseout', () => {
     allMainPixels.forEach((item, i) => {
-        item.classList.remove(listClassesForApplyBottom[i])
+        item.classList.remove(listClassesForApplyButton_main[i])
+    });
+
+    allBottomPixels.forEach((item, i) => {
+        item.classList.remove(listClassesForApplyButton_bottom[i])
     });
 });
 
 btnJury.addEventListener('mouseover', () => {
     allMainPixels.forEach((item, i) => {
-        item.classList.add(listClassesForJuryBottom[i])
+        item.classList.add(listClassesForJuryButton_main[i])
+    });
+
+    allBottomPixels.forEach((item, i) => {
+        item.classList.add(listClassesForJuryButton_bottom[i])
     });
 });
 
 btnJury.addEventListener('mouseout', () => {
     allMainPixels.forEach((item, i) => {
-        item.classList.remove(listClassesForJuryBottom[i])
+        item.classList.remove(listClassesForJuryButton_main[i])
+    });
+
+    allBottomPixels.forEach((item, i) => {
+        item.classList.remove(listClassesForJuryButton_bottom[i])
     });
 });
-
-
