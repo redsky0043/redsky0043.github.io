de08c319dbbd3ed05427b0a54490b5a3ac26bf10
Serhii Boiko <s.boiko@techiia.com>
2021-07-18 14:16:55 +0300

    restructuring

--- Test Tasks/2__Test_SuperClinic/js/dropdown.js
+++
@@ -1,7 +0,0 @@
-dropDownOpen = () => {
-    const dropBack = document.querySelector('.drop-down').style.display='inherit';
-}
-
-dropDownClose = () => {
-    const dropBack = document.querySelector('.drop-down').style.display='none';
-}
--- Test Tasks/4__Test_Swivl/js/main.js
+++
@@ -1,7 +0,0 @@
-
-const dropDown = document.querySelector('.dropdown').classList;
-const buttonDropDown = document.querySelector('.user__top__btn__img');
-
-buttonDropDown.addEventListener('click', () => {
-    dropDown.toggle('active');
-})
